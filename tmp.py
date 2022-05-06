00dfa2848a9dff39944e038f942efcc7c0cbe24b
Awes Mubarak <contact@awesmubarak.com>
2022-05-06 15:19:42 +0100

    Normalise headings

--- tmp.py
+++ tmp.py
@@ -1,4 +1,5 @@
 import wiktionary_polyglot as wp
 
-URL = wp.main.create_URL("fr", "fr", "nous")
-wp.main.parse_URL(URL)
+# URL = wp.main.create_URL("fr", "fr", "nous")
+URL = wp.main.create_URL("en", "en", "sheep")
+wp.main.parse_URL(URL, "en")
